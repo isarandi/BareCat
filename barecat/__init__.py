4537b3e9bb241bca988033be6112e129e70d51ef
István Sárándi <sarandi@vision.rwth-aachen.de>
2024-11-26 22:42:45 +0100

    Expose get_cached_reader

--- barecat/__init__.py
+++ barecat/__init__.py
@@ -2,5 +2,6 @@ from barecat.barecat import Reader, Writer
 from barecat.indexing import IndexReader, IndexWriter
 from barecat.barecat_unif import BareCat
 from barecat.indexing_unif import Index
+from barecat.threadsafe import get_cached_reader
 from barecat.util import read_index, write_index, create_from_stdin_paths, extract, merge, \
     merge_symlink, create_from_stdin_paths_workers
